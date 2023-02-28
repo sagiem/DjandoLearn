8dfa9f34bfa4768ab62755b718b1bc6c187e572d
Maksim Kashin <sagiems@gmail.com>
2023-02-28 11:35:08 +0600

    Добавил модель Item

--- core/models.py
+++ core/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
 
+class Item(models.Model):
+    name = models.CharField('Название', max_length=255, blank=True)
+    description = models.TextField('Описание', help_text='подсказка, если применимо')
+    priority = models.IntegerField('Приоритет сортировки', default=1)
+    bone = models.DateTimeField('Выполено', null=True, blank=True)
+    create = models.DateTimeField('Создан', auto_now_add=True)
+    update = models.DateTimeField('Обновлен', auto_now=True)
+
+    class Meta:
+        verbose_name = 'Дело'
+        verbose_name_plural = 'Список дел'
+
+
+
 class Person(models.Model):
     name = models.CharField('Имя', max_length=255)
     phone = models.IntegerField('Номер телефона')
