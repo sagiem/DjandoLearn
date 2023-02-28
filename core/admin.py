5fd0ed38d2c3b723e559dc6096e30532bf19a1da
Maksim Kashin <sagiems@gmail.com>
2023-02-28 15:46:43 +0600

    добавлены классы в models

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
 from core import models
 
-# Register your models here.
-@admin.register(models.Person)
-class Person(admin.ModelAdmin):
-    list_display = ('name', 'phone')
+class ItemResultInLine(admin.TabularInline):
+    model = models.ItemResult
+
+
+
+@admin.register(models.Item)
+class Item(admin.ModelAdmin):
+    inlines = (ItemResultInLine, )
+    list_display = ('name', 'done')
     search_fields = ('name',)
+
+# @admin.register(models.Tag)
+# class Tag(admin.ModelAdmin):
+#     inlines = (ItemResultInLine, )
+#     list_display = ('name', 'done')
+#     search_fields = ('name',)
+
+
+# @admin.register(models.Person)
+# class Person(admin.ModelAdmin):
+#     list_display = ('name', 'phone')
+#     search_fields = ('name',)
--- core/models.py
+++ core/models.py
@@ -1,17 +1,38 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
+User = get_user_model()
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+
 class Item(models.Model):
-    name = models.CharField('Название', max_length=255, blank=True)
+    user = models.ForeignKey(User, verbose_name='Поьзователь', on_delete=models.CASCADE, related_name='items')
+    name = models.CharField('Название', max_length=255, blank=True, unique=True)
     description = models.TextField('Описание', help_text='подсказка, если применимо')
-    priority = models.IntegerField('Приоритет сортировки', default=1)
-    bone = models.DateTimeField('Выполено', null=True, blank=True)
-    create = models.DateTimeField('Создан', auto_now_add=True)
-    update = models.DateTimeField('Обновлен', auto_now=True)
+    priority = models.IntegerField('Приоритет сортировки', default=1, db_index=True)
+    done = models.DateTimeField('Выполено', null=True, blank=True)
+    created = models.DateTimeField('Создан', auto_now_add=True, db_index=True)
+    updated = models.DateTimeField('Обновлен', auto_now=True)
+    tags = models.ManyToManyField(Tag, verbose_name='Метки', blank=True, related_name='items')
 
     class Meta:
         verbose_name = 'Дело'
         verbose_name_plural = 'Список дел'
+        ordering = ('priority', 'created')
+
+    def __str__(self):
+        return self.name
+
+
+class ItemResult(models.Model):
+    item = models.OneToOneField(Item, on_delete=models.CASCADE)
+    image = models.ImageField()
+
+
 
 
 
