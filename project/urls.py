6d21d4045d7ead32953367b0d6b970c7d4be6ca3
Maksim Kashin <sagiems@gmail.com>
2023-03-22 14:39:32 +0600

    изменил url

--- project/urls.py
+++ project/urls.py
@@ -6,7 +6,7 @@ from rest_framework_swagger.views import get_swagger_view
 schema_view = get_swagger_view(title='API')
 
 urlpatterns = [
-    path('', schema_view),
+    # path('', schema_view),
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
